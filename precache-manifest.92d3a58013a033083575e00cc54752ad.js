3c127b7479964558d709af520f826362bac11e26
Xinjn <422075616@qq.com>
2021-02-24 23:07:07 +0800

    deploy

---
+++ precache-manifest.92d3a58013a033083575e00cc54752ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f095b52aa8a57345cea",
+    "url": "/money-test-vue-website/css/app.0fc6c8dd.css"
+  },
+  {
+    "revision": "2c2719a6e8c83d0c045533de283aaefe",
+    "url": "/money-test-vue-website/index.html"
+  },
+  {
+    "revision": "6f095b52aa8a57345cea",
+    "url": "/money-test-vue-website/js/app.d8846c0c.js"
+  },
+  {
+    "revision": "f18cfcc12e5198afd63a",
+    "url": "/money-test-vue-website/js/chunk-vendors.93805bc4.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-test-vue-website/manifest.json"
+  },
+  {
+    "revision": "e20cfff2939b399df457e67c5f02cb97",
+    "url": "/money-test-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-test-vue-website/robots.txt"
+  }
+]);
